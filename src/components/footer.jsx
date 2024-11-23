f278c25d55cbd70b0af927bc1618dbd545b0bfb5
lajiuxiaoxin <86972225+liuxin03014@users.noreply.github.com>
2024-11-23 14:03:40 +0800

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
         <p>
           Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
           &nbsp;
-          <CustomLink to="https://www.imsyy.top/" text="無名" />
+          <CustomLink to="https://hong.15641300.xyz/" text="初九" />
           {siteIcp ? (
             <React.Fragment>
               &nbsp;|&nbsp;
